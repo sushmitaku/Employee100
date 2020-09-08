bc96b04bab05dbeb64ee791229eebe5b0b08eacb
sushmitaku <ksushmita1999@gmail.com>
2020-09-08 23:43:22 +0530

    WagePerhour and fullDayHour added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,14 +1,25 @@
 echo "Welcome to Emplyoee Wage Computation"
 #UC1 program is added Employee present or not"
 
-#!/bin/bash 
+#!/bin/bash
 
+wageperhour=20
+fulldayHour=8 
 isPresent=1
 randomCheck=$(( RANDOM%2 ))
 if [ $isPresent -eq $randomCheck ]
 then
-        echo "Employee uis present"
+	echo "Employee is present"
+        salary=$(( $wageperhour*$fulldayHour ))
+        echo $salary
 else
-        echo "Not presnet"
+        echo "employee absent"
+        echo "salary=0"
+
 fi
+
+
+
+
+
 
