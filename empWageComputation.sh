e241fd28c4c1c21278c3e9860b3ce1d36c02a080
sushmitaku <ksushmita1999@gmail.com>
2020-09-08 16:54:05 +0530

    Added:  Employee Attendance Functionality added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,14 @@
 echo "Welcome to Emplyoee Wage Computation"
+#UC1 program is added Employee present or not"
+
+#!/bin/bash 
+
+isPresent=1
+randomCheck=$(( RANDOM%2 ))
+if [ $isPresent -eq $randomCheck ]
+then
+        echo "Employee uis present"
+else
+        echo "Not presnet"
+fi
+
