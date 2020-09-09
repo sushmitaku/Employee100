891e6982a361d7961e5b927329c80870719b5ad2
sushmitaku <ksushmita1999@gmail.com>
2020-09-09 13:48:00 +0530

    Code Refactor

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,16 +1,23 @@
 echo "Welcome to Emplyoee Wage Computation"
 #UC1 program is added Employee present or not"
 
-#!/bin/bash
+#!/bin/bash -x
 
-wageperhour=20 
-randomCheck=$(( RANDOM%3 ))
-case $randomCheck in
-	0) Hour=0 ;;
-        1) Hour=8 ;;
-        2) Hour=16 ;;
+IsFullTime=2
+IsPartTime=1
+wageperhour=20
+EmployeeCheck=$(( RANDOM%3 ))
+case $EmployeeCheck in
+	$IsFullTime)
+		 Hour=16
+		 ;;
+        $IsPartTime)
+		 Hour=8
+		 ;;
 	*)
-	echo "wrong input"
+		Hour=0
+		;;
+
 esac
 salary=$(( $Hour*$wageperhour ))
 echo $salary
